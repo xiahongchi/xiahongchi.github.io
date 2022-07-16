8d9c3534611c21c9d14098282ea14bbfc25e7b6e
unknown <xiahongchi@sjtu.edu.cn>
2022-07-16 18:01:37 +0800

    first commit

---
+++ learning-site/test-site/scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src', 'images/cat.jpg');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myButton.onclick = function () {
+  setUserName();
+}
+
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hi cat ' + myName;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hi cat ' + storedName;
+}
+
